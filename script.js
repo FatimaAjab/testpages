77f687bdb872a2b23d28e5410e922903e180a7a8
FatimaAjab <faadumofani054@gmail.com>
2025-05-13 18:22:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function showAlert(message) {
+    alert(message);
+  }
+  
+  function submitForm() {
+    alert("Thank you for contacting us!");
+    return false; // prevent real submission
+  }
+  function goToPage() {
+  window.location.href = "about.html";
+}
+function goToPage2() {
+  window.location.href = "contact.html";
+}
+
